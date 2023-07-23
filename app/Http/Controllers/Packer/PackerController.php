80b74cce7d12de95b7900bdf4e3c42a12d80bb42
AlexeyRyzhakov <ryzhakovalexeynicol@gmail.com>
2023-07-23 14:08:08 +0300

    initial

---
+++ app/Http/Controllers/Packer/PackerController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Packer;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PackerController extends Controller
+{
+    //
+}
---
+++ app/Http/Controllers/Storekeeper/StoreKeeperController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Storekeeper;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class StoreKeeperController extends Controller
+{
+    //
+}
